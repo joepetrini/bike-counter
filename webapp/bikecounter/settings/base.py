a1e6f201a0b6a9c45736a733f0f181555ffb6e33
Joe Petrini <joepetrini@gmail.com>
2014-03-19 09:45:07 -0400

    Updated requirements.  Restructured settings files

--- webapp/bikecounter/settings/base.py
+++ webapp/bikecounter/settings/base.py
@@ -53,17 +53,6 @@ ROOT_URLCONF = 'bikecounter.urls'
 
 WSGI_APPLICATION = 'bikecounter.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
---
+++ webapp/bikecounter/settings/local_copyme.py
@@ -0,0 +1,17 @@
+"""
+Copy this file to local.py and update any local specific settings
+It is not tracked in source control.
+"""
+
+import os
+from .base import BASE_DIR
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
