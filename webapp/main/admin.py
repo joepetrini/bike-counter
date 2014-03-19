3312e49d30ecac19943e6ca872406d4f007e28a9
Joe Petrini <joepetrini@gmail.com>
2014-03-19 12:21:46 -0400

    Added admin file for backend data mgmt

--- webapp/main/admin.py
+++ webapp/main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Organization)
+admin.site.register(Membership)
+admin.site.register(Location)
+admin.site.register(ValueSet)
+admin.site.register(Value)
+admin.site.register(Metric)
+admin.site.register(OrganizationMetrics)
+admin.site.register(Appointment)
+admin.site.register(Survey)
+admin.site.register(SurveyValue)
