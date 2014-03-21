629d03e08bff68ddaa5d653c92083ab7436fbda2
Joe Petrini <joepetrini@gmail.com>
2014-03-21 17:39:31 -0400

    Added load_locations command to pull from google sheet

---
+++ webapp/main/management/commands/load_locations.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+import gspread
+from main.models import Location, Organization
+
+
+class Command(BaseCommand):
+    args = 'google_user google_pass'
+    help = 'Load locations from google spreadsheet'
+
+    def handle(self, *args, **options):
+        org = Organization.objects.all()[0]
+        key = '0AgUErk7rE6TLdGJ0NE90U2pDVE9MRkZrU2hGR0FhRlE'
+        gc = gspread.login(args[0], args[1])
+        sht = gc.open_by_key(key).get_worksheet(0)
+        values = sht.get_all_values()
+        for v in values[1:]:
+            [name, type, lat, long] = v
+            loc, c = Location.objects.get_or_create(organization=org, name=name)
+            loc.type = type
+            loc.lat = lat
+            loc.long = long
+            loc.save()
