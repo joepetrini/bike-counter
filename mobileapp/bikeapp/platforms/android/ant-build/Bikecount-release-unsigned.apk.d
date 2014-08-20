250302ba0a83168e1ecb4da5a9064eacf973ffaa
Joe Petrini <joepetrini@gmail.com>
2014-08-20 10:50:24 -0400

    Added keystore for android apk building

---
+++ mobileapp/bikeapp/platforms/android/ant-build/Bikecount-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Users/joepetrini/Projects/bikecounter/mobileapp/bikeapp/platforms/android/ant-build/Bikecount-release-unsigned.apk : \
+/Users/joepetrini/Projects/bikecounter/mobileapp/bikeapp/platforms/android/ant-build/Bikecount.ap_ \
+/Users/joepetrini/Projects/bikecounter/mobileapp/bikeapp/platforms/android/ant-build/classes.dex \
+/Users/joepetrini/Projects/bikecounter/mobileapp/bikeapp/platforms/android/CordovaLib/ant-build/classes.jar \
