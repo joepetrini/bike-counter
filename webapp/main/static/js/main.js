729b8c75ebbbdc7c6f43f54fde98869c088257b6
joepetrini <joepetrini@gmail.com>
2015-02-17 17:17:01 -0500

    Confirm dialog on session reset

--- webapp/main/static/js/main.js
+++ webapp/main/static/js/main.js
@@ -1,6 +1,9 @@
 function reset_appt(id){
-    $.get('/api/session/'+id+'/reset', function(data) {
-        $('#rst_span_'+id).fadeOut().html('Not started').fadeIn();
-        $('#rst_btn_'+id).fadeOut().remove();
-    });
+    var r = confirm("This action will delete all survey data for this count session");
+    if (r == true) {
+        $.get('/api/session/' + id + '/reset', function (data) {
+            $('#rst_span_' + id).fadeOut().html('Not started').fadeIn();
+            $('#rst_btn_' + id).fadeOut().remove();
+        });
+    }
 }
